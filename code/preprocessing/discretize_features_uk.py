3abf336aa970377043275c1b647a2a80bfd7a35f
JPanes <joepanes@outlook.com>
2020-11-16 16:42:28 +0000

    Update discretize_features_uk.py
    Able to read a .csv file

--- code/preprocessing/discretize_features_uk.py
+++ code/preprocessing/discretize_features_uk.py
@@ -1,3 +1,5 @@
+import csv
+import sys
 
 SEVERITY = {
     "low" : 1,
@@ -25,3 +27,42 @@ REGIONS = {
     "wales" : 12,
 }
 
+fileName = "../../data/uk_data.csv"
+optFileName = "../../data/uk_mlTable_0_0.csv"
+
+def main():
+    """
+    Converts the UK dataset into a format that is usuable in the next stage
+
+    INPUT:
+        NONE
+    
+    OUTPUT:
+        returns nothing, but creates writes the converted UK dataset to another .csv file
+    """
+    newLabel = [
+        'Day', 'Date', 'Regions', 'Cases', 'Cumulative cases',
+        'Deaths', 'Cumulative Deaths', 'Tests', 'Cumulative Tests',
+        'Meeting Friends/Family', 'Domestic Travel', 'Cafes and restaurants',
+        'Pubs and bars', 'Sports and leisure', 'Schools Closure'
+    ]
+    #readLabel = True
+    
+    rawData = []
+
+
+    with open(fileName, "r") as dataFile:
+        myReader = csv.DictReader(dataFile)
+
+        for row in myReader:
+            """if readLabel:
+                readLabel = False
+                
+                newLabel = list(row.keys())
+                continue"""
+
+            rawData.append(row)
+            
+
+if __name__ == '__main__':
+    sys.exit(main()) 
