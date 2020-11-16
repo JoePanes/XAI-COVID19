3b3b480d404efdd6248958874d48555aabec7392
JPanes <joepanes@outlook.com>
2020-11-16 15:26:20 +0000

    Update discretize_features_uk.py
    Added constants for the conversion of strings to integers

---
+++ code/preprocessing/discretize_features_uk.py
@@ -0,0 +1,27 @@
+
+SEVERITY = {
+    "low" : 1,
+    "moderate" : 2,
+    "high" : 3,
+}
+
+CLOSURE = {
+    "opened" : 0,
+    "closed" : 1,
+}
+
+REGIONS = {
+    "east midlands" : 1,
+    "east of england" : 2,
+    "london" : 3,
+    "north east" : 4,
+    "north west" : 5,
+    "south east" : 6,
+    "south west" : 7,
+    "west midlands" : 8,
+    "yorkshire and the humber" : 9,
+    "northern ireland" : 10,
+    "scotland" : 11,
+    "wales" : 12,
+}
+
