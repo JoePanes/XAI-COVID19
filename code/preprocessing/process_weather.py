433241bd2c015b68a514f982ac52b191ebc004c3
JPanes <joepanes@outlook.com>
2020-11-18 10:44:38 +0000

    Expanded program to work with all currently specified EU datasets
    What has been done:
    The program now prints a little progress bar to give visual feedback on whats what.
    Adjustments were made to how the file is read due to incompatibilities found in the encoder used by the original method with some datasets.
    Now round the average temp and humidity to 2 d.p.

--- code/preprocessing/process_weather.py
+++ code/preprocessing/process_weather.py
@@ -1,3 +1,11 @@
+"""
+Processes all of the .csv files present within the raw weather folder
+and extracts the temperature and humidity, averages all of their values for a
+given date then writes them into the processed folder.
+
+Uses weather data from this website: https://rp5.ru/Weather_archive_in_Chisinau
+"""
+
 import csv
 import sys
 import statistics
@@ -22,8 +30,9 @@ def writeToFile(fileWriter, date, tempList, humidList):
     """
     processedDate = date
 
-    processedTemp = statistics.mean(tempList)
-    processedHumidity = statistics.mean(humidList)
+    #Average and round to 2 d.p.
+    processedTemp = round(statistics.mean(tempList), 2)
+    processedHumidity = round(statistics.mean(humidList), 2)
 
     fileWriter.writerow({
         "Date" : processedDate, 
@@ -33,6 +42,72 @@ def writeToFile(fileWriter, date, tempList, humidList):
     )
 
     return None, [], []
+
+def printProgressBar(currNo, totalNo):
+    """
+    Calculate the current progress through the files and print a visual representation
+
+    INPUT:
+        :param currNo: Integer, the number of files that have been looked at already
+        :param totalNo: Integer, the total number of files in the list of files
+    
+    OUTPUT:
+        returns nothing, but, prints the current progress out to the command line
+    """
+    #Calculate the percentage
+    progress = int((currNo / totalNo) * 10)
+
+    progressBar = ["-"]*10
+    for i in range(0, progress):
+        progressBar[i] = "#"
+
+
+    progressBar = "".join(progressBar)
+    print("Progress: ["+ progressBar + f"] {currNo} / {totalNo}")
+    
+def readFile(fileName, encoding):
+    """
+    Reads in the given file using the specified encoder, then orders the rows to match the desired dataset.
+
+    INPUTS:
+        :param fileName: String, the name of the current file to be read
+        :param encoding: String, either UTF-8 or cp1252
+
+    OUTPUT:
+        returns the rows of the read file, ordered to match the desired dataset.
+                Along with the column where temperature and humidity are located
+    """
+
+    rawData = []
+    with open(filePath + fileName +".csv","r", encoding=encoding) as dataFile:
+            #Skip over rows that are not part of the database
+            for _ in range(6):
+                next(dataFile)
+
+            myReader = csv.reader(dataFile)
+            readLabel = True
+            
+            tempIndex = None
+            humidityIndex = None
+
+            for row in myReader:
+                if readLabel:
+                    #Find where the temperature and humidity are within the table
+                    i = 0
+                    for currElem in row:
+                        if currElem == "T":
+                            tempIndex = i
+                        elif currElem == "U":
+                            humidityIndex = i
+                        elif tempIndex != None and humidityIndex != None:
+                            break
+                        i += 1 
+                    readLabel = False
+                else:
+                    #Reverse the order of the data to match the EU dataset
+                    rawData.insert(0, row)
+
+    return rawData, tempIndex, humidityIndex
 
 def main():
     """
@@ -52,41 +127,28 @@ def main():
     for currFile in fileList:
         if currFile[-4::] == ".csv":
             inputFiles.append(currFile[:-4:])
-
-    print(inputFiles)
     
+    totalFiles = len(inputFiles)
+    
+    print(f"Found {totalFiles} files")
+    
+    currFileNo = 1
+
     for currFileName in inputFiles:
-        print(f"-----{currFileName}-----")
+        printProgressBar(currFileNo, totalFiles)
         rawData = []
         
-        tempIndex = None
-        humidityIndex = None
+        
 
-        with open(filePath + currFileName +".csv","r") as dataFile:
-            #Skip over rows that are not part of the database
-            for _ in range(6):
-                next(dataFile)
+        rawData = []
 
-            myReader = csv.reader(dataFile)
-            readLabel = True
-            
-            for row in myReader:
-                if readLabel:
-                    #Find where the temperature and humidity are within the table
-                    i = 0
-                    for currElem in row:
-                        if currElem == "T":
-                            tempIndex = i
-                        elif currElem == "U":
-                            humidityIndex = i
-                        elif tempIndex != None and humidityIndex != None:
-                            break
-                        i += 1 
-                    readLabel = False
-                else:
-                    #Reverse the order of the data to match the EU dataset
-                    rawData.insert(0, row)
-            
+        #Some of the datasets despite claming to be UTF-8 encoded, are not, hence this is necessary
+        try:
+            rawData, tempIndex, humidityIndex = readFile(currFileName, "UTF-8")
+        except:
+            rawData, tempIndex, humidityIndex = readFile(currFileName, "cp1252")
+
+
         with open(optFilePath + currFileName + "_processed.csv", "w") as optFile:
             myWriter = csv.DictWriter(optFile, ["Date", "Temp", "Humidity",])
 
@@ -108,10 +170,16 @@ def main():
                         currDateTemp.append(float(row[tempIndex]))
                         currDateHumid.append(float(row[humidityIndex]))
                     except:
-                        print(f"Invalid data at: {row[0]} in Temp (T) or Humididty (U) column")
+                        """
+                        This tends to occur when a feature within a dataset has no value
+                        
+                        However, it seems to be that only one or two readings may have this occur on a specific date
+                        and has very little impact.
+                        """
                             
             writeToFile(myWriter, currDate, currDateTemp,currDateHumid)
             
+        currFileNo +=1 
 
 if __name__ == '__main__':
     sys.exit(main()) 
