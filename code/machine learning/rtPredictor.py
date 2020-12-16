816d4d0679d9c6cb2a376af554e10bad229418af
JPanes <joepanes@outlook.com>
2020-12-16 10:59:45 +0000

    Update rtPredictor.py
    Put each part of creating the potential action list into functions.
    
    Starting working on the next step of implementing an agent.

--- code/machine learning/rtPredictor.py
+++ code/machine learning/rtPredictor.py
@@ -1,9 +1,12 @@
+import sys
+
 from csv import DictReader
 from pandas import qcut
 from pandas import DataFrame
 from statistics import mean
 
 QUANTILE_NO = 8
+
 filePath = "../../data/core/uk/Rt/uk_Rt.csv"
 
 def readFile(dataset, filePath):
@@ -49,80 +52,132 @@ def readFile(dataset, filePath):
 
     return compiledData
 
-#Read in Rt csv file
-regionRt = readFile("uk",filePath)
-print(len(regionRt))
-print(len(regionRt[0]))
-rtValueChange = []
-currIndex = 0
+def getRtValueChange(regionRt):
+    """
+    From the dataset, go through each regional sublist and obtain the Rt value change from
+    one day to the next.
 
-#Get the Rt value changes
-for currRegion in regionRt:
-    rtValueChange.append([])
-    prevRt = float(0)
+    INPUT:
+        :param regionRt: List of Lists, containing only the Rt values for each day in each region
     
-    for currRow in currRegion:
-        currRt = float(currRow)
+    OUTPUT:
+        returns a new list of lists, in the same structure and lengths as the input, but with the Rt values
+                now being the difference in value between one day to the next.
+    """
+    rtValueChange = []
+    currIndex = 0
 
-        rtValueChange[currIndex].append(float(currRt - prevRt))
-
-        prevRt = currRt
-    
-    currIndex += 1
-
-print(len(rtValueChange))
-print(len(rtValueChange[0]))
-"""for currIndex in range(len(rtValueChange[0])):
-    print(f"{regionRt[0][currIndex]} : {rtValueChange[0][currIndex]}")"""
+    #Get the Rt value changes
+    for currRegion in regionRt:
+        rtValueChange.append([])
+        prevRt = float(0)
         
+        for currRow in currRegion:
+            currRt = float(currRow)
 
-#Convert the Rt values to positive values and remove 0 values
-for currRegionIndex in range(len(rtValueChange)):
-    newRegionRtValueChange = []
+            rtValueChange[currIndex].append(float(currRt - prevRt))
+
+            prevRt = currRt
+        
+        currIndex += 1
+    return rtValueChange
+
+def convertRtValues(rtValueChange):
+    """
+    Converts all negative values to positive and remove 0 from the dataset.
+
+    INPUT:
+        :param rtValueChange: List of lists, the day to day value change of Rt
     
-    for currIndex in range(len(rtValueChange[currRegionIndex])):
-        currVal = rtValueChange[currRegionIndex][currIndex]
-        if currVal < 0:
-            newRegionRtValueChange.append(-currVal)
-        elif currVal == 0:
-            continue
-        else:
-            newRegionRtValueChange.append(currVal)
-
-    rtValueChange[currRegionIndex] = newRegionRtValueChange
-
-print(len(rtValueChange))
-print(len(rtValueChange[0]))
-regionalPotentialActionLists = []
-
-for currRegionIndex in range(len(rtValueChange)):
-    #Convert to a dataframe to easily perform quantile bucketing
-    rtValChangeDataFrame = DataFrame({"change" : rtValueChange[currRegionIndex]}, columns=["change"])
-
-    rtValChangeDataFrame["Bucket"] = qcut(rtValChangeDataFrame.change, QUANTILE_NO, labels=list(range(QUANTILE_NO)))
-    #The discretized bucket vals for each record
-    quantileBuckets = list(rtValChangeDataFrame["Bucket"])
-
-    #Where the Rt val changes will be sorted based on quantileBuckets
-    quantileBucketsValues = [[] for _ in range(QUANTILE_NO)]
+    OUTPUT:
+        returns the altered rtValueChange, which may have sublists now shorter.
+    """
+    for currRegionIndex in range(len(rtValueChange)):
+        newRegionRtValueChange = []
     
-    #Sort the Rt values into their corresponding bucket
-    for currIndex in range(len(rtValueChange[currRegionIndex])):
-        bucketVal = quantileBuckets[currIndex]
-        quantileBucketsValues[bucketVal].append(rtValueChange[currRegionIndex][currIndex])
+        for currIndex in range(len(rtValueChange[currRegionIndex])):
+            currVal = rtValueChange[currRegionIndex][currIndex]
+            if currVal < 0:
+                newRegionRtValueChange.append(-currVal)
+            elif currVal == 0:
+                continue
+            else:
+                newRegionRtValueChange.append(currVal)
 
-    actionList = [0,]
+        rtValueChange[currRegionIndex] = newRegionRtValueChange
     
-    #Average each of the buckets to get the action list
-    for currBucket in quantileBucketsValues:
-        actionList.append(mean(currBucket))
+    return rtValueChange
 
+def getPotentialActionLists(rtValueChange):
+    """
+    For each region, it performs Quantile Bucketing as a method,
+    of splitting a region's Rt values in an equal manner.
+    Then, derives the action list using this split data.
 
-    #For each of the postive potential actions add a negative version
-    for currVal in actionList[1:QUANTILE_NO+1]:
-        actionList.append(-currVal)
-    regionalPotentialActionLists.append(actionList)
+    INPUT:
+        :param rtValueChange: List of lists, the day to day value change of Rt
     
-print(regionalPotentialActionLists[17])
+    OUTPUT:
+        returns a list of lists, where each sublist contains the potential actions 
+                that can be taken for each region.
+    """
+    regionalPotentialActionLists = []
 
-    
+    for currRegionIndex in range(len(rtValueChange)):
+        #Convert to a dataframe to easily perform quantile bucketing
+        rtValChangeDataFrame = DataFrame({"change" : rtValueChange[currRegionIndex]}, columns=["change"])
+
+        rtValChangeDataFrame["Bucket"] = qcut(rtValChangeDataFrame.change, QUANTILE_NO, labels=list(range(QUANTILE_NO)))
+        #The discretized bucket vals for each record
+        quantileBuckets = list(rtValChangeDataFrame["Bucket"])
+
+        #Where the Rt val changes will be sorted based on quantileBuckets
+        quantileBucketsValues = [[] for _ in range(QUANTILE_NO)]
+        
+        #Sort the Rt values into their corresponding bucket
+        for currIndex in range(len(rtValueChange[currRegionIndex])):
+            bucketVal = quantileBuckets[currIndex]
+            quantileBucketsValues[bucketVal].append(rtValueChange[currRegionIndex][currIndex])
+
+        actionList = [0,]
+        
+        #Average each of the buckets to get the action list
+        for currBucket in quantileBucketsValues:
+            actionList.append(mean(currBucket))
+
+
+        #For each of the postive potential actions add a negative version
+        for currVal in actionList[1:QUANTILE_NO+1]:
+            actionList.append(-currVal)
+        regionalPotentialActionLists.append(actionList)
+    return regionalPotentialActionLists
+
+def greedyAgent(currRegionRtVal, currRegionPotentialActionList):
+    """
+    A greedy implementation for the agent to try and mimic as closely the
+    movements of the Rt values.
+
+    INPUTS:
+        :param currRegionRtVal: List of floats, all of a region's Rt values
+        :param currRegionPotentialActionList: List of floats, the changes in value that the agent will be able to use.
+
+    OUTPUTS:
+        returns a list of floats, with this being the Rt values that its chosen actions have produced
+    """
+    pass
+
+def main():
+    regionRt = readFile("uk",filePath)
+
+    rtValueChange = getRtValueChange(regionRt)
+
+    rtValueChange = convertRtValues(rtValueChange)
+
+    print(len(rtValueChange))
+    print(len(rtValueChange[0]))
+    regionalPotentialActionLists = getPotentialActionLists(rtValueChange)
+        
+    print(regionalPotentialActionLists[17])
+
+if __name__ == "__main__":
+    sys.exit(main())
